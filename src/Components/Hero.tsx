c9ce389ff4a680afc84c12af1bfb61aba60e9bb5
JATIN-RAGHAV <123885828+JATIN-RAGHAV@users.noreply.github.com>
2024-06-01 15:33:24 +0530

    Update Hero.tsx
    changed hero

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -11,8 +11,8 @@ const Hero = () => {
         <div className="w-screen h-screen bg-gray-800 flex justify-center items-center flex-col">
             <div className='w-[60%] h-[50%] bg-white rounded-2xl p-4 relative'>
                 <Image alt='' src={bg} className='rounded-2xl h-[40%] w-[100%]' />
-                <Image alt='' src={tejus} className='rounded-full h-[180px] w-[180px] absolute top-5 left-[40%] border-5' />
-                <h1 className='text-4xl font-bold text-center mt-12'>Tejus Gupta</h1>
+                <Image alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnoZyrVrm9yfqZGqzgnRJDQLqiPKdk9IX9lQ&s' className='rounded-full h-[180px] w-[180px] absolute top-5 left-[40%] border-5' />
+                <h1 className='text-4xl font-bold text-center mt-12'>Jatin Raghav</h1>
                 <p className='text-center mt-4'>Intern @ Kloudidev Digital Solution</p>
                 <div className='flex justify-center mt-4 gap-10'>
                     <a href='https://www.linkedin.com/in/tejus-gupta-3b7b5b1b0/' target='_blank' rel='noreferrer'>
